7dd5e0dbc09d3d2252b86fb922449bc69494d7c2
amch-med23 <skyied.med2023@gmail.com>
2023-10-28 20:43:58 +0100

    updating the code to adhere to betty-style rules

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -8,11 +8,11 @@
  */
 int infinite_while(void)
 {
-        while (1)
-        {
-                sleep(1);
-        }
-        return (0);
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
 }
 
 /**
@@ -21,17 +21,19 @@ int infinite_while(void)
  */
 int main(void)
 {
-        int i;
-        pid_t zombie;
+	int i;
+	pid_t zombie;
 
-        for (i = 0; i < 5; i++)
-        {
-                zombie = fork();
-                if (!zombie)
-                        return (0);
-                printf("Zombie process created, PID: %d\n", zombie);
-        }
+	for (i = 0; i < 5; i++)
+	{
+		zombie = fork();
+		if (!zombie)
+		{
+			return (0);
+		}
+		printf("Zombie process created, PID: %d\n", zombie);
+	}
 
-        infinite_while();
-        return (0);
+	infinite_while();
+	return (0);
 }
