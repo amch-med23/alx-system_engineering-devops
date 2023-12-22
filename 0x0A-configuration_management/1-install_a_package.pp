c3cee4e7c92b7d5b5c902e1f68a79863128a6e60
amch-med23 <skyied.med2023@gmail.com>
2023-12-23 00:26:49 +0100

    Task 1: adding the werkzeug package after flask, to ensure 'flask --version' has a correct output

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,10 @@
-# Installs flask using pipe3
+# Installs flask using pipe3 in the system
 
 package { 'flask':
   ensure   => '2.1.0',
+  provider => 'pip3',
+}
+package { 'werkzeug':
+  ensure   => '2.1.1',
   provider => 'pip3',
 }
