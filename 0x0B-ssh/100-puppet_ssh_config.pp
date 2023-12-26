1ff82bcbea915f55f342d89bdc19c6606a706942
root <root@DXU772>
2023-12-26 18:39:11 +0100

    TASK 4: adding the puppet file

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,17 @@
+# Configurng the SSH CLient file, as per requested.
+
+file { '/etc/ssh/ssh_config':
+     ensure => present,
+}
+
+file_line { 'Set private key':
+	  path => '/etc/ssh/ssh_config',
+	  line => 'IdentityFile ~/.ss h/school',
+	  match => '^IdentityFile'
+}
+
+file line { 'Deactivate password authentication':
+     	  path => '/stc/ssh/ssh_config',
+	  line => 'PasswordAuthentication no',
+	  match => '^PasswordAuthentication'
+}
