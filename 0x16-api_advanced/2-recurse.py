70c83bc2e5d4593f79862fb1c829e612450b54dc
amch-med23 <skyied.med2023@gmail.com>
2024-02-14 08:58:29 +0100

    using new method

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,51 +1,23 @@
 #!/usr/bin/python3
-"""Get list of titles recursevly"""
-import requests
-import time
 
-
-def helper(url, hot_list, after):
-    header = {'User-Agen': 'recurse_all_posts/1.0'}
-    payload = {'limit': 5, 'count': len(hot_list), 'after': after}
-    res = requests.get(url, headers=header,
-                       params=payload, allow_redirects=False)
-
-    if res.status_code == 429:
-        time.sleep(5)
-        helper(url, hot_list, after)
-
-    json = res.json()
-    data = json.get('data')
-
-    if not data or not data.get('before'):
-        return 0
-
-    if res.status_code == 200:
-        posts = data.get('children')
-        for post in posts:
-            hot_list.append(post.get('data').get('title'))
-
-        helper(url, hot_list, data.get('after'))
+"""a list containing the titles of all hot articles"""
 
 
 def recurse(subreddit, hot_list=[]):
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    header = {'User-Agen': 'recurse_all_posts/1.0'}
-    res = requests.get(url, headers=header,
-                       params={'limit': 5}, allow_redirects=False)
+    """ a recursive function that queries the Reddit API and returns a list
+    containing the titles of all hot articles for a given subreddit. If no
+    results are found for the given subreddit, the function should return None.
+    """
+    import requests
 
-    if res.status_code == 429:
-        time.sleep(5)
-        recurse(subreddit)
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {"User-agent": "amch-mozilla"}
 
-    json = res.json()
-    data = json.get('data')
+    posts = requests.get(url, headers=headers, allow_redirects=False)
 
-    if res.status_code == 200:
-        posts = data.get('children')
-        for post in posts[1:]:
-            hot_list.append(post.get('data').get('title'))
-        helper(url, hot_list, data.get('after'))
+    if posts.status_code == 200:
+        for post in posts.json()['data']['children']:
+            hot_list.append(post['data']['title'])
         return hot_list
     else:
-        None
+        return None
