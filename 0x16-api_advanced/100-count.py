4c12e842e9716d5e89c1c1880ab260bb9f451b18
amch-med23 <skyied.med2023@gmail.com>
2024-02-14 08:59:15 +0100

    Create 100-count.py

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""data from reddit api """
+
+import requests
+
+
+def count_words(subreddit, word_list):
+    """Returns counts of hot posts of a given subreddit."""
+    headers = {'User-agent': 'amch-mozilla'}
+    posts = requests.get('http://www.reddit.com/r/{}/hot.json'
+                         .format(subreddit), headers=headers)
+
+    if posts.status_code == 200:
+        posts = posts.json()['data']['children']
+        result = {word.lower(): 0 for word in word_list}
+        for post in posts:
+            title = post['data']['title'].lower()
+            for word in title.split(' '):
+                if word in result:
+                    result[word] += 1
+        return result
+    else:
+        return None
