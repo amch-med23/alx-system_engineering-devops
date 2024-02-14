e2b0d1b6fde4f2e4dd5a13005634ba4ec7bbf8a4
amch-med23 <skyied.med2023@gmail.com>
2024-02-14 08:56:30 +0100

    using new method.

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-"""Get subs count for a subreddit"""
+
+""" given Reddit API and returns the number of subscribers"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Number of subs"""
+    """ Reddit API and returns the number of subscribers"""
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    header = {'User-Agen': 'number_of_subs/1.0'}
-    res = requests.get(url, headers=header, allow_redirects=False)
-    if res.status_code == 200:
-        data = res.json()
-        return data.get('data').get('subscribers')
-
-    return 0
-    
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        return 0
+    return response.json().get('data').get('subscribers')
